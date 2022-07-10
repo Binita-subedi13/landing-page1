84e4d449c30decffcf2e5fe88be3a3411edd9bb6
Binita-subedi13 <sbinita0303@gmail.com>
2022-07-10 12:28:23 +0545

    frst commit

---
+++ script.js
@@ -0,0 +1,8 @@
+function openNav() {
+    document.getElementById("mySidebar").style="right:30px;";
+  }
+ 
+
+  function closeNav() {
+    document.getElementById("mySidebar").style="right:-250px;)";
+  }
